dd1af60e3fdf4659c7cee7811fdb6ec174fd43bd
Thomas Pappas <getekid@gmail.com>
2022-02-22 18:10:02 +0200

    Added shell script to automatically run the script for all inputs

---
+++ run_all_inputs.sh
@@ -0,0 +1,5 @@
+python one_pizza.py < input_data/a_an_example.in.txt > input_data/submissions/a_sub.txt
+python one_pizza.py < input_data/b_basic.in.txt > input_data/submissions/b_sub.txt
+python one_pizza.py < input_data/c_coarse.in.txt > input_data/submissions/c_sub.txt
+python one_pizza.py < input_data/d_difficult.in.txt > input_data/submissions/d_sub.txt
+python one_pizza.py < input_data/e_elaborate.in.txt > input_data/submissions/e_sub.txt
