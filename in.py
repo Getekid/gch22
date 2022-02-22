5eb17c070ad0b25ca68850d0fe9e3fd589fcefec
Thomas Pappas <getekid@gmail.com>
2022-02-22 16:32:46 +0200

    Improvements in input scripts

---
+++ in.py
@@ -0,0 +1,25 @@
+# from one_pizza import Client
+import time
+
+t0 = time.time()
+
+
+ingredients = set()
+C = input()
+
+clients = []
+for i in range(int(C)):
+    liked = input().split(' ')[1:]
+    for ing in liked:
+        ingredients.add(ing)
+
+    disliked = input().split(' ')[1:]
+    for ing in disliked:
+        ingredients.add(ing)
+
+    clients.append(Client(liked, disliked))
+
+# print(clients)
+print(len(ingredients))
+
+print('Time elapsed:', time.time() - t0)
--- inputCLmod.py
+++ inputCLmod.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from one_pizza import Client
 
 def flatten(lists):
    return [item for sublist in lists for item in sublist]
@@ -9,44 +10,40 @@ def flatten(lists):
 lists = []
 liked_pizza = []
 disliked_pizza = []
+input()
 for i, line in enumerate(sys.stdin):
     if i == 0:
         no_of_clients = line
         print(no_of_clients)
     if i != 0:
-        lists.append((line.rstrip('\n')).split(' ')) # lists.append([(line.rstrip('\n'))].split(' '))
-        #lists.append(line.split(' '))
-        #print(lists)
-        print('Lists before flatten() is ', lists)
-        ingredients = flatten(lists)
-        print('Lists after flatten() is ', ingredients)
+        lists.append((line.rstrip('\n')).split(' '))
         
         if i%2==1:
-            for j in range(1, len(lists), 1):						#lists instead of ingredients
-                liked_pizza.append(lists[i])
-                print('Ingredients in pizza, \'YES\': ', liked_pizza)
+            liked_pizza.append(lists[i - 1])
         elif i%2==0:
-            for j in range(1, len(lists), 1):
-                disliked_pizza.append(lists[i])
-                print('Ingredients in pizza, \'NO\': ', disliked_pizza)
-            
+            disliked_pizza.append(lists[i - 1])
 
-print(lists)
-
+print('Lists before flatten() is ', lists)
 ingredients = flatten(lists)
-print(ingredients)
-liked_pizza = []
-for i in range(1, len(ingredients), 1):
-    liked_pizza.append(ingredients[i]) 
+print('Lists after flatten() is ', ingredients)
 
-print('Liked pizza is :',liked_pizza)
-#ingredients = flatten(flatten(lists))
-set_ingredients = set(ingredients)
-print('Ingredients are :', ingredients)
-print('Ingredients, as a set, are :', set_ingredients)
+print('Liked pizza is :', liked_pizza)
+print('Disliked pizza is :', disliked_pizza)
+# #ingredients = flatten(flatten(lists))
+# set_ingredients = set(ingredients)
+# print('Ingredients are :', ingredients)
+# print('Ingredients, as a set, are :', set_ingredients)
     #for i, line in enumerate(file)
     #print(line, 'number of line is ', i )
 
 #for i in lists:
 #	if i%2==0
-#	lists[i] 
+#	lists[i]
+
+clients = []
+for j in range(len(liked_pizza)):
+    liked = liked_pizza[j]
+    disliked = disliked_pizza[j]
+    clients.append(Client(liked[1:], disliked[1:]))
+
+print(clients)
