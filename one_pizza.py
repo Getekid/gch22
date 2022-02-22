cf35abe9d322a20e8740dd9676ef8342ab7b28f0
Thomas Pappas <getekid@gmail.com>
2022-02-22 18:09:42 +0200

    Changed brute force algorithm to a greedy one

--- one_pizza.py
+++ one_pizza.py
@@ -7,6 +7,14 @@ class Client:
             dislikes = []
         self.likes = likes
         self.dislikes = dislikes
+        self.cost = 0
+
+    def dislikes_pizza(self, pizza):
+        """Returns True if the pizza has any of the ingredients the client dislikes, False otherwise."""
+        for ing in self.dislikes:
+            if ing in pizza:
+                return True
+        return False
 
     def likes_pizza(self, pizza):
         """Returns True if the client likes the given pizza, False otherwise."""
@@ -15,41 +23,57 @@ class Client:
             if ing not in pizza:
                 return False
         # None of the ingredients the Client dislikes should be in.
-        for ing in self.dislikes:
-            if ing in pizza:
-                return False
+        if self.dislikes_pizza(pizza):
+            return False
+
         # If all the above fail, return True.
         return True
 
     def __repr__(self):
-        return 'Client likes:' + str(self.likes) + ' Client dislikes' + str(self.dislikes)
+        return 'Client likes' + str(self.likes) + ' and dislikes' + str(self.dislikes)
 
 
-ingredients = set()
 C = input()
 
+ingredients_cost = {}
 clients = []
 for i in range(int(C)):
     liked = input().split(' ')[1:]
     for ing in liked:
-        ingredients.add(ing)
+        if ing not in ingredients_cost:
+            ingredients_cost[ing] = 0
 
     disliked = input().split(' ')[1:]
     for ing in disliked:
-        ingredients.add(ing)
+        ingredients_cost[ing] = 1 if ing not in ingredients_cost else ingredients_cost[ing] + 1
 
     clients.append(Client(liked, disliked))
 
-# Brute force, try all possible combinations and keep the one with the highest popularity.
-popularity_max = 0
-best_pizza = []
-for r in range(1, len(ingredients) + 1):
-    pizzas = combinations(ingredients, r)
-    for pizza in list(pizzas):
-        clients_liked_pizza = [clients[i].likes_pizza(pizza) for i in range(len(clients))]
-        popularity = sum(clients_liked_pizza)
-        if popularity_max < popularity:
-            popularity_max = popularity
-            best_pizza = pizza
+# Build a list of the added costs for each client.
+# This doesn't take into account overlaps, i.e. a client that dislikes two ingredients
+# will be counted twice for a client that likes both.
+for client in clients:
+    cost = 0
+    for ing in client.likes:
+        cost += ingredients_cost[ing]
+    client.cost = cost
+
+# Sort the clients according to their cost.
+clients_sorted = sorted(clients, key=lambda x: x.cost)
+
+# Take the ingredients from clients in order of lower cost and ignore the ones disliking the building pizza.
+# Also keep track of the disliked ingredients of the clients we accept and ignore those who like them.
+best_pizza = set()
+ingredients_banned = set()
+for client in clients:
+    if client.dislikes_pizza(best_pizza):
+        continue
+    if ingredients_banned and not ingredients_banned.intersection(set(client.likes)):
+        continue
+
+    for ing in client.likes:
+        best_pizza.add(ing)
+    for ing in client.dislikes:
+        ingredients_banned.add(ing)
 
 print(len(best_pizza), ' '.join(best_pizza))
