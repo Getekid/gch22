8f37f25b254d8ec688a1a3b1343649fe62d147a0
Thomas Pappas <getekid@gmail.com>
2022-03-03 08:31:47 +0200

    Added .gitignore and bash script to run for all inputs

---
+++ qual/run_all_inputs.sh
@@ -0,0 +1,6 @@
+python qual.py < input_data/a_an_example.in.txt > a_sub.txt
+python qual.py < input_data/b_better_start_small.in.txt > b_sub.txt
+python qual.py < input_data/c_collaboration.in.txt > c_sub.txt
+python qual.py < input_data/d_dense_schedule.in.txt > d_sub.txt
+python qual.py < input_data/e_exceptional_skills.in.txt > e_sub.txt
+python qual.py < input_data/f_find_great_mentors.in.txt > f_sub.txt
